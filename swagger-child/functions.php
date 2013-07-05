bb6069291545dd5ff339bd52145200a06644824a
Jason Bobich <jasonbobich@gmail.com>
2013-07-05 11:48:48 -0800

    Swagger sample child theme

---
+++ swagger-child/functions.php
@@ -0,0 +1,13 @@
+<?php
+/*-------------------------------------------------------*/
+/* Run Theme Blvd framework (required)
+/*-------------------------------------------------------*/
+
+require_once( get_template_directory() . '/framework/themeblvd.php' );
+
+/*-------------------------------------------------------*/
+/* Start Child Theme
+/*-------------------------------------------------------*/
+
+// Your actions and filters go here ...
+// See http://dev.themeblvd.com
