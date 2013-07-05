d37befe6157e5d6c4b1b10d60dc26f954128765f
Jason Bobich <jasonbobich@gmail.com>
2013-07-05 11:47:49 -0800

    Akita sample child theme

--- akita-child/functions.php
+++ akita-child/functions.php
@@ -8,3 +8,6 @@ require_once( get_template_directory() . '/framework/themeblvd.php' );
 /*-------------------------------------------------------*/
 /* Start Child Theme
 /*-------------------------------------------------------*/
+
+// Your actions and filters go here ...
+// See http://dev.themeblvd.com
